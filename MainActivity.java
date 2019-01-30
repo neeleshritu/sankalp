cf38af4b02ad51ee1cdca32ddcacac7c09832c4e
neeleshritu <47192024+neeleshritu@users.noreply.github.com>
2019-01-31 02:08:11 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,22 +1,6 @@
 package com.neeleshritu.fightforhunger;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.Html;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
