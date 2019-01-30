4ce260034b35e8a3b5df06fecf1e71719f94dbff
neeleshritu <47192024+neeleshritu@users.noreply.github.com>
2019-01-31 02:05:28 +0530

    Add files via upload

---
+++ donerlocActivity.java
@@ -0,0 +1,18 @@
+package com.neeleshritu.fightforhunger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class donerlocActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donerloc);
+
+        Toast.makeText(this, "all the data sucsessfully uploaded to firebase", Toast.LENGTH_SHORT).show();
+
+
+    }
+}
